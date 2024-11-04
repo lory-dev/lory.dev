77dda3d90ef1afc0bd4032406ddabc128d307805
Lory <briciola.mc@gmail.com>
2024-11-04 20:17:59 +0100

    push changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
+  output: "export",
+  basePath: "/lory.dev",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
