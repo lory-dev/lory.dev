f68b1a026118632dd514799d57e86650261697aa
Lory <briciola.mc@gmail.com>
2024-11-05 18:32:54 +0100

    i make stuff now apparently

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "lory.dev",
-  description: "game dev",
+  description: "I make stuff",
 };
 
 export default function RootLayout({
